59eb64b14266dd0e5982d1b52af954c93b816624
Rana Wajahat Saleem <128230044+Wajahat-Rana@users.noreply.github.com>
2023-12-07 23:16:51 +0500

    Added Quote

--- math-magicians/src/components/Quote.js
+++ math-magicians/src/components/Quote.js
@@ -3,8 +3,9 @@ import React from 'react'
 function Quote() {
     return (
         <div className='container' style={{border: "1px solid gray", borderRadius: "30px 0px 0px 0px", marginTop: "10%", padding: "20px"}}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa qui adipisci rerum quas sunt dolor repudiandae, nesciunt dolorem unde amet perferendis est in at praesentium veniam. Quisquam nulla ullam ipsum!</p>
-            <p>By : Author</p>
+            <p>Mathematics as an expression of the human mind reflects the active will, the contemplative reason, and the desire for aesthetic perfection. Its basic elements are logic and intuition, analysis and construction, generality and individuality.
+            </p>
+            <p>— Richard Courant, German-American mathematician</p>
         </div>
     )
 }
